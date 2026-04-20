9ef81ed0ff97cc2e79b17fdc523f513667aa528a
搁浅 <3298633414@qq.com>
2026-04-20 23:00:51 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-  cout<<"hello worle!"<<endl;
-  return 0;
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    return {i,j}; 
+                }
+            }
+        }
+        return {};
+    }
+};
