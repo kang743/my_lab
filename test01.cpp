305343a1adc1e3c6a79163d274e7b5aa36139b8c
搁浅 <3298633414@qq.com>
2026-04-27 17:06:43 +0800

    Update test01.cpp

--- test01.cpp
+++ test01.cpp
@@ -5,3 +5,39 @@ int main()
   cout<<"hello worle!"<<endl;
   return 0;
 }
+#include<iostream>
+using namespace std;
+class Animal
+{
+public:
+	virtual void speak()
+	{
+		cout << "动物说话" << endl;
+	}
+};
+
+class Cat:public Animal
+{
+public:
+	void speak()
+	{
+		cout << "小猫说话" << endl;
+	}
+};
+
+void dospeak(Animal& animal)
+{
+	animal.speak();
+}
+
+void test01()
+{
+	Cat cat;
+	dospeak(cat);
+}
+
+int main()
+{
+	test01();
+	return 0; 
+}
