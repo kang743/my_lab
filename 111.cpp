1c45dd8fb6cdf44e8808ad116d512add87fff7cc
搁浅 <3298633414@qq.com>
2026-05-07 13:28:46 +0800

    Create 111.cpp

---
+++ 111.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        int arr[100];
+        int i = 0;
+        while(x!=0)
+…                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
